5678c27d31470e4cc2f52e012540bfd4d6634691
NEM907 <nathanseaw@gmail.com>
2018-11-26 11:14:08 -0800

    Update sceneMain.js

--- sceneMain.js
+++ sceneMain.js
@@ -1,1 +1,23 @@
 //This is the Master File that will be ran in Code.org
+//When adding code please add a comment above it explaining what the code does and also your name 
+//Good Luck Have Fun (:
+
+//Sets the pen to default values - Nathan Martin
+function penDefault() {
+	penRGB(0, 0, 0);
+	penWidth(1);
+	penDown();
+	show();
+}
+x = getX();
+penDefault();
+
+//Creates fucntion to draw grid for Dev Use Only - Nathan Martin
+function drawDevGrid() {
+	penUp();
+	moveTo(0, 450);
+	for(i = x; i < 321; i = x) {
+	  moveForward(450);
+	}
+}
+drawDevGrid();
